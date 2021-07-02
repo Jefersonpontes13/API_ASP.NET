3c3c63f0e08416f78a232d753c4cdf90b21adad4
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-02 17:41:27 -0300

    Adiciona Swagger response

---
+++ ASP.NET_API/Models/ValidaCampoViewModelOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_API.Models
+{
+    public class ValidaCampoViewModelOutput
+    {
+        public IEnumerable<string> Erros { get; private set; }
+
+        public ValidaCampoViewModelOutput(IEnumerable<string> erros)
+        {
+            Erros = erros;
+        }
+    }
+}
