e16437e0428488a3693d43015b0b0c3f647a0fec
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-02 16:50:22 -0300

    Inicia Projeto de API - C# ASP.NET

---
+++ ASP.NET_API/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_API.Models.Usuarios;
+
+namespace ASP.NET_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
