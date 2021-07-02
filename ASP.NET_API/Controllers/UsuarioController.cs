dd6ee21094882801aaf10891b39ccf7bba244def
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-02 17:38:14 -0300

    Adiciona SwaggerResponse
    ErroGenerico
    ValidaCampos

--- ASP.NET_API/Controllers/UsuarioController.cs
+++ ASP.NET_API/Controllers/UsuarioController.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASP.NET_API.Models;
 using ASP.NET_API.Models.Usuarios;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace ASP.NET_API.Controllers
 {
@@ -13,11 +15,15 @@ namespace ASP.NET_API.Controllers
     public class UsuarioController : ControllerBase
     {
         /// <sumary>
-        /// teste
+        /// Este serviço permite autenticar um usuário cadastrado e ativo.
         /// </sumary>
-        /// <param name="loginViewModelInput"></param>
-        /// <returns></returns>
-
+        /// <param name="loginViewModelInput">View Model do Login</param>
+        /// <returns>retorna Status okm dados do usuário e o token em caso de sucesso</returns>
+        
+        
+        [SwaggerResponse(statusCode: 200, description:"Sucesso ao Autenticar", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description:"Campos Obrigatórios", Type = typeof(ValidaCampoViewModelOutput))]
+        [SwaggerResponse(statusCode: 500, description:"Erro Interno", Type = typeof(ErroGenericoViewModel))]
         [HttpPost]
         [Route("logar")]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
