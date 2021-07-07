50b1e1ac9306369938d4c768d54d52454a848505
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-07-07 10:32:19 -0300

    Cria Projeto de Tests

---
+++ tests/ASP.NET_API.TESTS/Integrations/Controllers/UsuarioControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using ASP.NET_API.Models.Usuarios;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ASP.NET_API.TESTS.Integrations.Controllers
+{
+    public class UsuarioControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _httpClient;
+        public UsuarioControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.CreateClient();
+        }
+        
+        [Fact]
+        public void Logar()
+        {
+            var loginViewModelInput = new LoginViewModelInput
+            {
+                Login = "jeferson",
+                Senha = "pass"
+            };
+            StringContent content = new StringContent(JsonConvert.SerializeObject(loginViewModelInput));
+            var httpClientRequest = _httpClient.PostAsync("api/v1/usuario/logar", content).GetAwaiter().GetResult();
+            
+            Assert.Equal(System.Net.HttpStatusCode.OK, httpClientRequest.StatusCode);
+        }
+    }
+}
